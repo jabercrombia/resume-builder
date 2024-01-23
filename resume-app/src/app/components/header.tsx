65f94430b0000fcd2575044712c89638315588b6
Justin Abercrombia <jabercrombia@gmail.com>
2024-01-23 10:39:42 -0600

    open graph

--- resume-app/src/app/components/header.tsx
+++ resume-app/src/app/components/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
     return(
     <div className="container mx-auto">
         <GoogleAnalytics gaId="G-ZD8JDCF4DQ" />
-        <h1 className="text-2xl">Resume Builder</h1>
+        <h1 style={{fontSize: 60}}>Resume Builder</h1>
         <p>Once you are done click <strong>Save Resume</strong> and click <strong>Download PDF</strong>.</p>
     </div>
     )
