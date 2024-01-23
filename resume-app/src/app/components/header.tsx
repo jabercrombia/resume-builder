9b0fee56f65ca7ae08b30fee4e4a69f23e324816
Justin Abercrombia <jabercrombia@gmail.com>
2024-01-23 09:21:52 -0600

    google analytics

--- resume-app/src/app/components/header.tsx
+++ resume-app/src/app/components/header.tsx
@@ -3,7 +3,7 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 export default function Header() {
     return(
     <div className="container mx-auto">
-        <GoogleAnalytics gaId="CKPL5K50S8" />
+        <GoogleAnalytics gaId="G-ZD8JDCF4DQ" />
         <h1 className="text-2xl">Resume Builder</h1>
         <p>Once you are done click <strong>Save Resume</strong> and click <strong>Download PDF</strong>.</p>
     </div>
