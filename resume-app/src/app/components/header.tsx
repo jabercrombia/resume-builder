065d46dfd13385604804633b28fc66c6e503e36f
Justin Abercrombia <jabercrombia@gmail.com>
2024-01-23 08:54:45 -0600

    move resume

--- resume-app/src/app/components/header.tsx
+++ resume-app/src/app/components/header.tsx
@@ -1,12 +1,11 @@
 import { GoogleAnalytics } from '@next/third-parties/google'
 
-
 export default function Header() {
     return(
-        
     <div className="container mx-auto">
         <GoogleAnalytics gaId="CKPL5K50S8" />
         <h1 className="text-2xl">Resume Builder</h1>
         <p>Once you are done click <strong>Save Resume</strong> and click <strong>Download PDF</strong>.</p>
-        </div>)
+    </div>
+    )
 }
